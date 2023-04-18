2f0643665b959a2952f43eb335a865a2d90c4182
Asia <jw429677@students.mimuw.edu.pl>
2023-04-18 12:18:38 +0200

    move func

--- 4/src/run.py
+++ 4/src/run.py
@@ -19,8 +19,30 @@ import sys
 sys.path.append('mingptdemo')
 from mingpt import model, utils
 
-from utils import evaluate_places
 
+def evaluate_places(filepath, predicted_places):
+  """ Computes percent of correctly predicted birth places.
+
+  Arguments:
+    filepath: path to a file with our name, birth place data.
+    predicted_places: a list of strings representing the
+        predicted birth place of each person.
+
+  Returns:
+    (total, correct), floats
+  """
+  with open(filepath) as fin:
+    print("here")
+    lines = [x.strip().split('\t') for x in fin]
+    if len(lines[0]) == 1:
+      print('No gold birth places provided; returning (0,0)')
+      return (0,0)
+    true_places = [x[1] for x in lines]
+    total = len(true_places)
+    assert total == len(predicted_places)
+    correct = len(list(filter(lambda x: x[0] == x[1],
+      zip(true_places, predicted_places))))
+    return (float(total),float(correct))
 
 argp = argparse.ArgumentParser()
 argp.add_argument('function',
@@ -59,6 +81,8 @@ pretrain_dataset = dataset.CharCorruptionDataset(text, block_size)
 # use them for both the vanilla and the synthesizer models
 mconf = model.GPTConfig(pretrain_dataset.vocab_size, pretrain_dataset.block_size,
     n_layer=4, n_head=8, n_embd=256)
+
+
 
 """
 Don't change above here; write your code below
